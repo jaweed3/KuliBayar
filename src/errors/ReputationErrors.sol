28e8a39653dc76689a32f6d8d8f90530d412acc0
jaweed3 <wedjaw22@gmail.com>
2026-07-10 14:22:48 +0700

    feat: error reputation init

---
+++ src/errors/ReputationErrors.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+error ProfileAlreadyExists();
+error ProfileNotFound();
+error NotProfileOwner();
+error InvalidRating();
+error NotAuthorized();
