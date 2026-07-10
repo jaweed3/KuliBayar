6c49f4b1e9b89fcc91ff2a13ea4fdb442dfb5220
jaweed3 <wedjaw22@gmail.com>
2026-07-10 14:22:39 +0700

    feat: error types

---
+++ src/errors/ProjectEscrowErrors.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+error NotAuthorized();
+error InsufficientFunds();
+error ProjectNotActive();
+error AlreadyCompleted();
+error NoFundsToRelease();
+error InvalidProjectId();
